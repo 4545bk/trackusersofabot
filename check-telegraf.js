02b357d411e7441ddf7f2e881403353c885d65ed
4545bk <natnaelgizawayano@gmail.com>
2024-10-12 17:27:43 +0300

    Initial commit

---
+++ check-telegraf.js
@@ -0,0 +1,7 @@
+try {
+    const { Telegraf } = require('telegraf');
+    console.log('Telegraf is installed and ready to use!');
+  } catch (error) {
+    console.error('Telegraf is not installed.');
+  }
+  
